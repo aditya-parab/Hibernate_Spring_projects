0a836870443eb7841f8a8c88b6bce437f112d1ea
aditya <adparab97+0@gmail.com>
2023-08-15 14:55:41 +0530

    removed check for validation of student, need to correct

---
+++ college-mgmt/src/main/java/com/example/collegemgmt/entities/Book.java
@@ -0,0 +1,63 @@
+package com.example.collegemgmt.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+        private String title;
+
+    @Column(nullable = false)
+    private String pages;
+
+    @Column(nullable = true)
+    private Double price;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String pages, Double price) {
+        this.title = title;
+        this.pages = pages;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
